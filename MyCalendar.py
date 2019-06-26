fb8679b9718d3baf70ac36ba12f15beb260cb5e5
ttkgt <37229851+ttkgt@users.noreply.github.com>
2019-06-26 17:12:33 +0900

    2019/06/26 14:36

--- MyCalendar.py
+++ MyCalendar.py
@@ -11,6 +11,19 @@ class mycalendar(tk.Frame):
 
         # 現在の日付を取得
         now = datetime.datetime.now()
+        # 現在の年と月を属性に追加
+        self.year = now.year
+        self.month = now.month
 
-
+        # frame_top部分の作成
+        frame_top = tk.Frame(self)
+        frame_top.pack(pady=5)
+        self.previous_month = tk.Label(frame_top, text = "<", font = ("",14))
+        self.previous_month.pack(side = "left", padx = 10)
+        self.current_year = tk.Label(frame_top, text = self.year, font = ("", 18))
+        self.current_year.pack(side = "left")
+        self.current_month = tk.Label(frame_top, text = self.month, font = ("",18))
+        self.current_month.pack(side = "left")
+        self.next_month = tk.Label(frame_top, text = ">", font = ("", 14))
+        self.next_month.pack(side = "left", pack = 10)
 
